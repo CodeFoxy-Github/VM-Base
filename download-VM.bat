57844ac80f74db2e5b9e306f237bee16a9351a2b
foxcat <96522178+foxcat-test@users.noreply.github.com>
2022-06-18 21:49:07 +0800

    Update download-VM.bat

--- download-VM.bat
+++ download-VM.bat
@@ -1,8 +1,14 @@
+rem //this is sync with ver 2.3.1
 @echo off
-echo warning!!!
-echo this only works in virtualbox!
+echo If VirtualBox is NOT installed enter "yes"
+echo else just press enter key
 timeout 3 /nobreak
 cls
+set /p ans="install VirtualBox6.1.34 ?"
+if %ans% == yes (
+winget install Oracle.VirtualBox
+)
+msiexec.exe /i %cd%\VirtualBox-6.1.34a-150636-Win.exe /QN /L*V "C:\Temp\msilog.log"
 echo 1. Windows 10 Home x64
 echo 2. Windows 10 Professional x64
 set /p id="Enter ID: "
@@ -31,7 +37,7 @@ cd /d %~dp0
 Call :UnZipFile "%USERPROFILE%\VirtualBox VMs" "%cd%\%id%.zip"
 
 :UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\_.vbs"
+set vbs="%temp%\0D5G0H8E0T9ZAH.vbs"
 if exist %vbs% del /f /q %vbs%
 >%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
 >>%vbs% echo If NOT fso.FolderExists(%1) Then
@@ -44,15 +50,5 @@ if exist %vbs% del /f /q %vbs%
 >>%vbs% echo Set objShell = Nothing
 cscript //nologo %vbs%
 if exist %vbs% del /f /q %vbs%
-
-
-
-
-
-
-
-
-
-
 echo install completed!
 timeout /t -1
