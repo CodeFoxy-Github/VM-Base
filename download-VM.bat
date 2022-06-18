f7413f0d97b62885d564643950375499238caac5
foxcat <96522178+foxcat-test@users.noreply.github.com>
2022-06-19 07:45:51 +0800

    Update download-VM.bat

--- download-VM.bat
+++ download-VM.bat
@@ -1,49 +1,30 @@
 rem //this is the latest version
 @echo off
-echo If VirtualBox is NOT installed enter "yes"
-echo else just press enter key
-timeout 3 /nobreak
-cls
-set /p ans="install VirtualBox6.1.34 ?"
-if %ans% == yes (
-winget install Oracle.VirtualBox
-)
-echo 1. Windows 10 Home x64
-echo 2. Windows 10 Professional x64
-set /p id="Enter ID: "
-if %id% == 1 (
-start "" "https://drive.google.com/uc?id=1SQHTssEtXWNppTeQH9IP2ds88DMhnY-E&confirm=t"
-)
-if %id% == 2 (
-start "" "https://drive.google.com/uc?id=1Z6vD5BNrMqKOWAt7iZ9txA37bYHPynCU&export=download&confirm=t"
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-echo if download is completed press any key to install
-timeout /t -1
-cls
-cd C:\Program Files\Oracle\VirtualBox
-if %id% == 1 (
-VBoxManage createvm --name "Windows 10 Home x64" --register
-)
-if %id% == 2 (
-VBoxManage createvm --name "Windows 10 Professional x64" --register
-)
-setlocal
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-cd /d %~dp0
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 
-Call :UnZipFile "%USERPROFILE%\VirtualBox VMs" "%cd%\%id%.zip"
-
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\0D5G0H8E0T9ZAH.vbs"
-if exist %vbs% del /f /q %vbs%
->%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")rem //this is the latest version
-@echo off
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
 echo If VirtualBox is NOT installed enter "yes"
 timeout 3 /nobreak
 cls
@@ -87,15 +68,6 @@ if exist %vbs% del /f /q %vbs%
 >>%vbs% echo fso.CreateFolder(%1)
 >>%vbs% echo End If
 >>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
-echo install completed!
-timeout /t -1
-
 >>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
 >>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
 >>%vbs% echo Set fso = Nothing
